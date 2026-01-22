ec0e922514e2930625ec9fbf6eaa6ef74b761428
Jacob Vosmaer <contact@jacobvosmaer.nl>
2026-01-22 12:12:49 +0100

    fzlist: add comments and bounds checks

--- fzlist.c
+++ fzlist.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #define NSECTOR 1280
 #define SECTORSIZE 1024
-uint8_t disk[NSECTOR * SECTORSIZE + 1];
+uint8_t disk[NSECTOR * SECTORSIZE + 1], *diskend = disk + sizeof(disk) - 1;
 char *PROGNAME = "fzlist", *types = "FVBESP";
 int u16(uint8_t *p) { return ((int)p[1] << 8) + (int)p[0]; }
 int main(int argc, char **argv) {
@@ -21,13 +21,20 @@ int main(int argc, char **argv) {
     fail("invalid image size: %d", n);
   printf("File: %s\nLabel: %12.12s\n", argv[1], disk);
   for (p = disk + SECTORSIZE; *p && p < disk + 2 * SECTORSIZE; p += 16) {
-    uint8_t *q = disk + SECTORSIZE * u16(p + 14), *qend = q + SECTORSIZE;
-    int s0, s1;
+    /* An FZ-1 file is a series of ranges of sectors. This allows the FZ-1 to
+     * re-use sectors after a file has been deleted. To find out the size of a
+     * file we must sum the sizes of the ranges. Variable q points to the first
+     * range in the extent table of the current file. */
+    uint8_t *q = disk + SECTORSIZE * u16(p + 14), *qend = q + SECTORSIZE,
+            filetype = p[12];
+    if (!(q < diskend && qend <= diskend))
+      fail("invalid file head address at offset %ld", p - disk);
     n = 0;
-    for (s0 = u16(q), s1 = u16(q + 2); s0 && q < qend;
-         q += 4, s0 = u16(q), s1 = u16(q + 2))
-      n += s1 - s0 + 1;
-    printf("%c  %9d  %12.12s\n", types[p[12]], n * SECTORSIZE, p);
+    for (; u16(q) && q < qend; q += 4)
+      n += u16(q + 2) - u16(q) + 1;
+    if (filetype > 5)
+      fail("invalid filetype at offset %ld", p - disk);
+    printf("%c  %9d  %12.12s\n", types[filetype], n * SECTORSIZE, p);
   }
   return 0;
 }
