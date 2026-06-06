c6543683dc57aae3246decc8f3cd7f49fde5fa70
Jacob Vosmaer <contact@jacobvosmaer.nl>
2026-06-06 13:28:03 +0200

    int.c: fix get32

--- int.c
+++ int.c
@@ -1,7 +1,7 @@
 #include "int.h"
 uint16_t get16(uint8_t *p) { return ((uint16_t)p[1] << 8) | (uint16_t)p[0]; }
 uint32_t get32(uint8_t *p) {
-  return (uint32_t)get16(p + 2) | (uint32_t)get16(p);
+  return ((uint32_t)get16(p + 2) << 16) | (uint32_t)get16(p);
 }
 void put16(uint8_t *p, uint16_t x) {
   p[0] = x;
