33eab8973ee604ce08365ef27b17c7da0798e2f5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2026-06-09 09:15:42 +0200

    fzformat: use named constants instead of magic numbers

--- fzformat.c
+++ fzformat.c
@@ -18,17 +18,20 @@ int main(int argc, char **argv) {
   snprintf((char *)sector, 13, "%-12.12s", argv[1]);
   sector[14] = 2;
   snprintf((char *)sector + 16, 13, "%-12.12s", argv[1]);
-  sector[128] = 3; /* First two clusters are allocated */
+#define CATstart 128
+  sector[CATstart] = 3; /* First two clusters are allocated */
   /* The Cluster Allocation Table has 6144 entries but the physical disk has
    * only 1280 clusters. So the last 4864 clusters are marked as allocated. */
-  memset(sector + 0x120, 0xff, sizeof(sector) - 0x120);
+#define NSECTOR 1280
+#define CATunused (CATstart + NSECTOR / 8)
+  memset(sector + CATunused, 0xff, sizeof(sector) - CATunused);
   if (!fwrite(sector, sizeof(sector), 1, f))
     fail("fwrite sector 0");
   memset(sector, 0, sizeof(sector));
   if (!fwrite(sector, sizeof(sector), 1, f))
     fail("fwrite sector 1");
   memset(sector, 'Z', sizeof(sector));
-  for (i = 2; i < 1280; i++)
+  for (i = 2; i < NSECTOR; i++)
     if (!fwrite(sector, sizeof(sector), 1, f))
       fail("fwrite sector %d", i);
   if (fclose(f))
